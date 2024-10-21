2a166d66968114b7e55f3983d60c0f262e5ab7ad
joschonarth <joschonarth@gmail.com>
2024-10-21 20:38:03 -0300

    Create Repository for Student Certification

--- src/main/java/com/joschonarth/certification_project/modules/students/useCases/VerifyCertificationUseCase.java
+++ src/main/java/com/joschonarth/certification_project/modules/students/useCases/VerifyCertificationUseCase.java
@@ -1,14 +1,20 @@
 package com.joschonarth.certification_project.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joschonarth.certification_project.modules.students.dto.VerifyCertificationDTO;
+import com.joschonarth.certification_project.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyCertificationUseCase {
     
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyCertificationDTO dto) {
-        if(dto.getEmail().equals("joschonarth@gmail.com") && dto.getTechnology().equals("Java")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if(!result.isEmpty()) {
             return true;
         }
         return false;
