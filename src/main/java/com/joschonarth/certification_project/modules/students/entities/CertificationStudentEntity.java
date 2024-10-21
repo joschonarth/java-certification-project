72ef84a8d94c3e7eb09d34a900207bcb1f792ef9
joschonarth <joschonarth@gmail.com>
2024-10-21 19:49:29 -0300

    Mapped entities to the database

--- src/main/java/com/joschonarth/certification_project/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/joschonarth/certification_project/modules/students/entities/CertificationStudentEntity.java
@@ -3,6 +3,17 @@ package com.joschonarth.certification_project.modules.students.entities;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,13 +21,29 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @JoinColumn(name = "student_id")
     private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grade;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
     List<AnswersCertificationsEntity> answersCertificationsEntities;
 
-    
 }
