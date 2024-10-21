1a7ef38a36665d7b8288b25697030767fa1315aa
joschonarth <joschonarth@gmail.com>
2024-10-21 15:29:53 -0300

    Certification Project

---
+++ src/main/java/com/joschonarth/certification_project/CertificationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.joschonarth.certification_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joschonarth/certification_project/CertificationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joschonarth.certification_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
