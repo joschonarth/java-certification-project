a36dba7da49941ee47cbb667588cf7c4720c597d
joschonarth <joschonarth@gmail.com>
2024-10-21 22:25:21 -0300

    Add seed for questions and alternatives

---
+++ src/main/java/com/joschonarth/certification_project/modules/questions/entities/AlternativesEntity.java
@@ -0,0 +1,34 @@
+package com.joschonarth.certification_project.modules.questions.entities;
+
+import java.util.UUID;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "alternatives")
+public class AlternativesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+}
